7b2557e6361f7dd832c601a72334707a1462787f
3bderhman <abderhmansaid58@gmail.com>
2021-09-27 21:09:22 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 let today = document.getElementById("today");
 let date = document.getElementById("date");
-let submitInput = document.getElementById("submit");
 let locationInput = document.querySelector(".locations");
 let regions = document.querySelector(".region");
 let TemperatureElement = document.querySelector(".temperature");
@@ -8,6 +7,7 @@ let minimalGraetTemperatureElement = document.querySelector(".minimal-graet-temp
 let descriptionWeatherElement = document.querySelector(".description-weaher");
 let windSpeedElement = document.querySelector(".wind-speed");
 let humidityElement = document.querySelector(".humidity")
+let tempImageElement = document.getElementById("temp-image");
 
 
 
@@ -38,18 +38,54 @@ function disPlay() {
     let descriptionWeather = weatherDetails.weather[0].description;
     let windSpeed = weatherDetails.wind.speed;
     let humidity = weatherDetails.main.humidity;
+    let tempImage = weatherDetails.weather[0].icon;
 
     regions.innerHTML = `<i class="fas fa-map-marker-alt"></i>${country}`;
     TemperatureElement.innerHTML = `${Math.floor(Temperature)}*C`;
     minimalGraetTemperatureElement.innerHTML = `${Math.ceil(graetTemperature)}*C/${Math.floor(minimalTemperature)}*C`;    
     descriptionWeatherElement.innerHTML = descriptionWeather;
     windSpeedElement.innerHTML = `Wind Speed: ${windSpeed}`;
-    humidityElement.innerHTML = `Humidity: ${humidity}`
+    humidityElement.innerHTML = `Humidity: ${humidity}`;
+    tempImageElement.innerHTML = `<img id="temp-image" src=" http://openweathermap.org/img/wn/${tempImage}@2x.png" alt="image">`
 }
-submitInput.addEventListener("click", function() {
-    let location = locationInput.value;
-    getWeather(location)
-})
+// function disPlayimage() {
+//     let tempImage = weatherDetails.weather[0].icon;
+//     let tempId = weatherDetails.weather[0].id;
+//     if (tempId>=200 && tempId<=232) {
+//         tempImageElement.innerHTML = `<img id="temp-image" src=" http://openweathermap.org/img/wn/11d@2x.png" alt="image">`
+//     }
+//     else if (tempId>=300 && tempId<=321 || tempId>=520 && tempId<=531) {
+//         tempImageElement.innerHTML = `<img id="temp-image" src=" http://openweathermap.org/img/wn/09d@2x.png" alt="image">`
+//     }
+//     else if (tempId>=500 && tempId<=504) {
+//         tempImageElement.innerHTML = `<img id="temp-image" src=" http://openweathermap.org/img/wn/10d@2x.png" alt="image">`
+//     }
+//     else if (tempId>=600 && tempId<=622 || tempId==511) {
+//         tempImageElement.innerHTML = `<img id="temp-image" src=" http://openweathermap.org/img/wn/13d@2x.png" alt="image">`
+//     }
+//     else if (tempId>=701 && tempId<=781) {
+//         tempImageElement.innerHTML = `<img id="temp-image" src=" http://openweathermap.org/img/wn/50d@2x.png" alt="image">`
+//     }
+//     else if (tempId>=800) {
+//         tempImageElement.innerHTML = `<img id="temp-image" src=" http://openweathermap.org/img/wn/${tempImage}@2x.png" alt="image">`
+//     }
+//     else if (tempId==800) {
+//         tempImageElement.innerHTML = `<img id="temp-image" src=" http://openweathermap.org/img/wn/${tempImage}@2x.png" alt="image">`
+//     }
+//     else if (tempId==801) {
+//         tempImageElement.innerHTML = `<img id="temp-image" src=" http://openweathermap.org/img/wn/${tempImage}@2x.png" alt="image">`
+//     }  
+//     else if (tempId==802) {
+//         tempImageElement.innerHTML = `<img id="temp-image" src=" http://openweathermap.org/img/wn/${tempImage}@2x.png" alt="image">`
+//     }   
+//     else if (tempId==803) {
+//         tempImageElement.innerHTML = `<img id="temp-image" src=" http://openweathermap.org/img/wn/${tempImage}@2x.png" alt="image">`
+//     }
+//     else if (tempId==804) {
+//         tempImageElement.innerHTML = `<img id="temp-image" src=" http://openweathermap.org/img/wn/${tempImage}@2x.png" alt="image">`
+//     }
+    
+// }
 locationInput.addEventListener("keyup", function() {
     let location = locationInput.value;
     getWeather(location)
